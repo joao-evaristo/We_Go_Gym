a29a7a7bca8d684150e4f1a51c81c267a0bf41e1
joao-evaristo <joao.evaristojw@gmail.com>
2022-10-26 10:02:33 -0300

    update carrossel + adicoes homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_130608) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_134933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "nome"
+    t.integer "idade"
+    t.text "telefone"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
