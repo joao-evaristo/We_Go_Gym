744a725914c01837d32cff18474c915b36417bc4
gaschrader <gaschrader02@gmail.com>
2022-11-30 21:51:55 -0300

    Fix new user

--- app/frontend/entrypoints/register.js
+++ app/frontend/entrypoints/register.js
@@ -30,13 +30,13 @@ const register_validation = createApp({
                 this.msg['email'] = ''
             }
             else {
-                this.msg['email'] = 'Insira um endereço de email válido'
+                this.msg['email'] = ' (Insira um endereço de email válido).'
             }
         },
         validatePassword(value){
             let difference = 8 - value.length
             if (value.length < 8){
-                this.msg['password'] = `No mínimo 8 caracteres! ${difference} caracteres restantes`
+                this.msg['password'] = ` (No mínimo 8 caracteres! ${difference} caracteres restantes).`
             }
             else {
                 this.msg['password'] = ''
@@ -55,7 +55,7 @@ const register_validation = createApp({
                 this.msg['phone_number'] = ''
             }
             else {
-                this.msg['phone_number'] = 'Insira um número de telefone válido'
+                this.msg['phone_number'] = ' (Insira um número de telefone válido).'
             }
         }
     }
